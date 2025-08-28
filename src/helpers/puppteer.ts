c9d26537974508250b415c542b1ec64df7e1da45
Ikaro Bruno Da Mata Montanari <ikaro.montanari@db1.com.br>
2025-08-28 16:15:00 -0400

    implementando melhorias na instalação do rpa

--- src/helpers/puppteer.ts
+++ src/helpers/puppteer.ts
@@ -8,6 +8,7 @@ export async function initPuppeteer() {
 
     const browser = await puppeteer.launch({
         headless: true,
+        executablePath: puppeteer.executablePath(),
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
